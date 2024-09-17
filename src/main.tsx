63fcae3ce1eeeafb83478cf73e8cbbb06ef90ba2
col3name <mmikusho@instaloper.com>
2024-09-17 20:17:31 +0300

    feat: minor refactor of components

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+
 import './index.css'
+
+import App from './App'
+
 import {StoreProvider} from "./app/stores";
 
 createRoot(document.getElementById('root')!).render(
