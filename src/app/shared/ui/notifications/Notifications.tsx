4366505af0f07d4c1d6a94f4a0c37fca19942ec5
col3name <mmikusho@instaloper.com>
2024-09-17 22:23:11 +0300

    feat: remove framer-motion for decrease bundle size

--- src/app/shared/ui/notifications/Notifications.tsx
+++ src/app/shared/ui/notifications/Notifications.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Children, isValidElement } from 'react';
 import cn from 'classnames';
-import {AnimatePresence} from "framer-motion";
+// import {AnimatePresence} from "framer-motion";
 
 import styles from './Notifications.module.scss';
 
 import {INotificationProps} from "../../typings/notifications";
 
-import {SlideAnimation} from "../animation/slideAnimation";
+// import {SlideAnimation} from "../animation/slideAnimation";
 
 export interface NotificationsProps
     extends React.HTMLAttributes<HTMLUListElement> {
@@ -21,29 +21,36 @@ export const Notifications: React.FC<NotificationsProps> =
         ...restProps
     }) => (
         <ul className={ cn(styles.notifications, className) } { ...restProps }>
-            <AnimatePresence>
-                { Children.map(children, child => {
-                    if (!isValidElement(child)) {
-                        return null;
-                    }
+            {/*<AnimatePresence>*/}
+            {/*    { Children.map(children, child => {*/}
+            {/*        if (!isValidElement(child)) {*/}
+            {/*            return null;*/}
+            {/*        }*/}
 
-                    return (
-                        <SlideAnimation
-                            key={ child.props.id }
-                            component="li"
-                            id={ child.props.id }
-                            layout
-                            className={ styles.item }
-                            direction="down"
-                            value={ 50 }
-                            inPercents={ false }
-                            exitDirection="opposite"
-                            withFading
-                        >
-                            { child }
-                        </SlideAnimation>
-                    );
-                }) }
-            </AnimatePresence>
+            {/*        return (*/}
+            {/*            <SlideAnimation*/}
+            {/*                key={ child.props.id }*/}
+            {/*                component="li"*/}
+            {/*                id={ child.props.id }*/}
+            {/*                layout*/}
+            {/*                className={ styles.item }*/}
+            {/*                direction="down"*/}
+            {/*                value={ 50 }*/}
+            {/*                inPercents={ false }*/}
+            {/*                exitDirection="opposite"*/}
+            {/*                withFading*/}
+            {/*            >*/}
+            {/*                { child }*/}
+            {/*            </SlideAnimation>*/}
+            {/*        );*/}
+            {/*    }) }*/}
+            {/*</AnimatePresence>*/}
+            { Children.map(children, child => {
+                if (!isValidElement(child)) {
+                    return null;
+                }
+
+                return null;
+            }) }
         </ul>
     );
